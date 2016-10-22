becd6b596cc338d866a0ce3666c693eabda0ac2b
N03048839 <giannitj2@hawkmail.newpaltz.edu>
2016-10-22 16:07:23 -0400

    0.3 -- implemented input commands, command parsing

--- src/T3Client.java
+++ src/T3Client.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 
 public class T3Client 
-{
-	private final int BOARD_SIZE;
-	private final int BOARD_DIM;
-	
+{	
 	final char icon;
+	final int pid;
+	
+	boolean isAdmin = false;
+	String name;
+	
 	
 	protected PrintStream screen = null;
 	protected Scanner kb = null;
 	
 	
-	public T3Client(final int boardSize, final int boardDim, final char icon) 
+	public T3Client(final int playerID, final char icon) 
 	{
-		this.BOARD_SIZE = boardSize;
-		this.BOARD_DIM = boardDim;
+		this.pid = playerID;
 		this.icon = icon;
 		
 		screen = System.out;
 		kb = new Scanner( System.in );
+		
+		this.name = prompt("Welcome to tic-tac-toe! Enter your name.");
 	}
 	
 	
@@ -29,20 +32,42 @@ public class T3Client
 	 * Gets a move from the player.
 	 * @return board index corresponding to the player's move
 	 */
-	int prompt(String message)
-	{
-		/* Get player's move */
+	String prompt(String message)
+	{	
 		screen.print("\n" + message + "\n> ");
-		int s = kb.nextInt();
-		
-		/* Parse player input */
-		
-		return s;
+		return kb.nextLine();
 	}
 	
+	int promptInt(String message)
+	{
+		screen.print("\n" + message + "\n> ");
+		String s = kb.nextLine();	// using nextLine() for all input prevents unpleasant problems
+		return Integer.parseInt(s);
+	}
+	
+	String name() {
+		return this.name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
+	void setAdmin(boolean a) {
+		this.isAdmin = a;
+	}
+	
+	boolean isAdmin() {
+		return this.isAdmin;
+	}
 	
 	void print(String s)
 	{
 		screen.print(s);
+	}
+	
+	void close()
+	{
+		kb.close();
 	}
 }
--- src/T3Server.java
+++ src/T3Server.java
@@ -1,4 +1,4 @@
-import java.io.PrintStream;
+import java.util.Arrays;
 
 
 
@@ -7,18 +7,24 @@ public class T3Server implements Runnable
 	private static final int BOARD_SIZE = 4;
 	private static final int BOARD_DIM = 2;
 	private static final char EMPTY_SPACE = '-';
+	private static final int MAX_PLAYERS = 10;
+	private static final int SPAWN_CNT = 2;
 	
+	private static final String ADMIN_PASSW = "password";
 	private static final String WIN_MESSAGE = "Congratulations! You win!";
 	private static final String LOSS_MESSAGE = "You lose";
 	private static final String TIE_MESSAGE = "Game over -- Tie";
+	private static final int SYSTEM = -1;		// used to indicate non-player messages
+	
 	
 	private T3Client[] clients;
-	private int clientCount = 1;
+	private int clientCount = 0;
 	
 	private char[] board;
 	
 	private boolean gameOver = false;
 	
+	private int tokenIterator;
 	
 	
 	/**
@@ -28,9 +34,11 @@ public class T3Server implements Runnable
 	{	
 		final char A = 'A';
 		
-		clients = new T3Client[clientCount];
-		for (int i = 0; i < clients.length; i++)
-			clients[i] = new T3Client(BOARD_SIZE, BOARD_DIM, (char)(A+i));
+		clients = new T3Client[MAX_PLAYERS];
+		for (int i = 0; i < SPAWN_CNT; i++) {
+			clients[i] = new T3Client(i, (char)(A+i));
+			clientCount++;
+		}
 		
 	}
 	
@@ -66,13 +74,12 @@ public class T3Server implements Runnable
 		
 		/* End-of-game actions */
 		for (int cl = 0; cl < clients.length; cl++)
-			if ( clients[cl].prompt("Would you like to play again?") == 0) {
-				clients[cl].print("Thanks for playing!");
-				clientCount--;
-				clients[cl] = null;
+			if ( clients[cl] != null && 
+				clients[cl].promptInt("Would you like to play again?") == 0) {
+				removePlayer(clients[cl], false);
 			}
 		
-		if (clientCount > 0)
+		if (clientCount > 1)
 			run();
 	}
 	
@@ -85,34 +92,56 @@ public class T3Server implements Runnable
 	{
 		for (int cl = 0; cl < clients.length; cl++)
 		{	
-			char icon = clients[cl].icon;
+			if ( gameOver ) return;		// Needed if game ends before cycling through players
+			if (clients[cl] != null)
+			{
 			
-			/* If human player, show board */
-			clients[cl].print(drawBoard());
-			
-			/* Get player's move */
-			int move = clients[cl].prompt("Enter a move.");
-			place(move, icon);
-			
-			/* Check if player won */
-			if ( checkforWin(icon)) {
-				gameOver = true;
-				clients[cl].print(drawBoard() + WIN_MESSAGE);
-				for (int cl2 = 0; cl2 < clients.length; cl2++)
-					if (clients[cl] != clients[cl2])
-						clients[cl2].print(drawBoard() + LOSS_MESSAGE);
-				return;
-			}
-			
-			/* Check for tie */
-			if ( checkforTie()) {
-				gameOver = true;
-				for (int cl2 = 0; cl2 < clients.length; cl2++)
-					clients[cl2].print(drawBoard() + TIE_MESSAGE);
-				return;
+				char icon = clients[cl].icon;
+				
+				/* If human player, show board */
+				clients[cl].print(drawBoard());
+				
+				
+				/* Get player's move */
+				boolean validMove = false;
+				while (!validMove) 
+				{
+					tokenIterator = 0;
+					String[] tokens = clients[cl].prompt("Enter a move.").split(" ");
+					
+					/* Test: display tokenized command string */
+					if (tokens.length == 0)
+						System.out.println("Error tokenizing input string: zero tokens!");
+					else {
+						System.out.print("\nTokenized input string: " + tokens[0]);
+						for (int i = 1; i < tokens.length; i++)
+							System.out.print(" + " + tokens[i]);
+						System.out.println();
+					}
+					
+					/* Parse and execute any user commands */
+					validMove = parseCommand(clients[cl], tokens);
+				}
+				
+				
+				/* Check if player won */
+				if ( checkforWin(icon)) {
+					gameOver = true;
+					clients[cl].print(drawBoard() + WIN_MESSAGE);
+					for (int cl2 = 0; cl2 < clients.length; cl2++)
+						if (clients[cl] != clients[cl2])
+							clients[cl2].print(drawBoard() + LOSS_MESSAGE);
+					return;
+				}
+				
+				/* Check for tie */
+				if ( checkforTie()) {
+					gameOver = true;
+					messageAll(SYSTEM, TIE_MESSAGE);
+					return;
+				}
 			}
 		}
-		return;
 	}
 	
 	
@@ -218,9 +247,309 @@ public class T3Server implements Runnable
 	
 	
 	
+	/**
+	 * Gets a move from the player.
+	 * 
+	 * REGULAR:
+	 * enter move
+	 * quit game
+	 * display board
+	 * display commands/help
+	 * query game stats
+	 * message all players
+	 * message one player
+	 * request admin priviledge
+	 * 
+	 * ADMIN:
+	 * boot one player
+	 * force game over
+	 * query player stats
+	 * 
+	 * @return board index corresponding to the player's move
+	 */
+	boolean parseCommand(T3Client player, String[] commandList)
+	{
+		if (tokenIterator >= commandList.length)
+			return false;
+		
+		
+		
+		/* Case: request admin priviledge */
+		if (commandList[tokenIterator].matches("sudo|admin")) 
+		{
+			tokenIterator++;
+			if (player.prompt("[sudo] enter admin password:").matches(ADMIN_PASSW)) {
+				player.setAdmin(true);
+				player.print("You now have sudo access.");
+			}
+			return false;
+		}
+		
+		
+		
+		/* Case: force game over */
+		if (commandList[tokenIterator].matches("kill|gameover"))
+		{
+			tokenIterator++;
+			if (!player.isAdmin) {
+				if (player.prompt("[kill] enter admin password:").matches(ADMIN_PASSW))
+					player.setAdmin(true);
+			}
+			
+			if (tokenIterator < commandList.length
+					&& commandList[tokenIterator++].matches("-" + "[Kk]")) { 	// end of command list
+				messageAll(SYSTEM, "[gameover: kill] Program terminated");
+				System.exit(0);
+			}
+			
+			gameOver = true;
+			return true;
+		}
+		
+		
+		
+		
+		/* Case: boot player */
+		if (commandList[tokenIterator].matches("boot"))
+		{
+			tokenIterator++;
+			if (tokenIterator >= commandList.length) { 	// end of command list
+				player.print("[boot] error: no player specified");
+				return false;
+			}
+			
+			if (!player.isAdmin) {
+				if (player.prompt("[boot] enter admin password:").matches(ADMIN_PASSW))
+					player.setAdmin(true);
+			}
+			String targetName = commandList[tokenIterator++];
+			int target;
+			try {
+				target = Integer.parseInt(targetName);
+			} catch (NumberFormatException e) {
+				player.print("[boot] Error: illegal boot target \"" + targetName + "\"");
+				return false;
+			}
+			if (player.promptInt("Booting player: " + clients[target].name() + "\nContinue? (1/0)") == 1) {
+				removePlayer(target, true);
+				return (player.pid == target || clientCount < 2);
+			}
+			return false;
+		}
+		
+		
+		
+		/* Case: player leaves game */
+		if (commandList[tokenIterator].matches("quit"))
+		{
+			tokenIterator++;
+			if ( (tokenIterator < commandList.length && commandList[tokenIterator++].matches("-" + "[Ss]") )
+					|| player.promptInt("Quit game? (1/0)") == 1)
+			{
+				removePlayer(player, false);
+				return true;
+			}
+		}
+		
+		
+		/* Case: Player requests a board re-draw */
+		if (commandList[tokenIterator].matches("board"))
+		{
+			tokenIterator++;
+			player.print(drawBoard());
+			return false;
+		}
+		
+		
+		/* Case: Player requests game stats */
+		if (commandList[tokenIterator].matches("gamestats"))
+		{
+			tokenIterator++;
+			player.print(gameStats());
+			return false;
+		}
+		
+		
+		/* Case: Player requests player stats */
+		if (commandList[tokenIterator].matches("playerstats"))
+		{
+			tokenIterator++;
+			player.print(playerStats());
+			return false;
+		}
+		
+		
+		/* Case: Player requests 'help' menu */
+		if (commandList[tokenIterator].matches("help|commands"))
+		{
+			tokenIterator++;
+			player.print(helpScreen());
+			return false;
+		}
+		
+		
+		/* Case: Player sends message */
+		if (commandList[tokenIterator].matches("-m|message"))
+		{
+			tokenIterator++;
+			if (commandList.length < 2) {
+				player.print("[message] Error: not enough arguments provided");
+				return false;
+			}
+			
+			String target = commandList[tokenIterator++];
+			if (target.matches("all")) {
+				messageAll(player.pid, Arrays.toString(commandList));
+				return false;
+			}
+			else {
+				int targetID = Integer.parseInt(target);	// Error here means player target wasn't entered as int
+				message(player.pid, targetID, Arrays.toString(commandList));
+				return false;
+			}
+		}
+		
+		/* Case: player places token */
+		if (commandList[tokenIterator].matches("[0-9]+"))
+		{
+			int target = Integer.parseInt(commandList[tokenIterator++]);
+			return place(target, player.icon) == 0;
+		}
+		
+		player.print("Invalid input -- for a list of commands, type \'help\'.");
+		return false;
+	}
+	
 	
 	/**
-	 * Creates a string representation of this game's gameboard for display.
+	 * Remove a player from the game.
+	 * @param player
+	 */
+	void removePlayer(T3Client player, boolean boot)
+	{
+		int playerID = player.pid;
+		
+		clientCount--;
+		clients[playerID] = null;
+		if (boot)
+		{
+			player.print("You have been booted.");
+			messageAll(SYSTEM, player.name() + " has been removed from the game.");
+		}
+		else 
+		{
+			player.print("Thanks for playing!");
+			messageAll(SYSTEM, player.name() + " has left the game.");
+		}
+		
+		
+		if (clientCount < 2)
+			gameOver = true;
+	}
+	
+	
+	void removePlayer(int playerID, boolean boot)
+	{
+		T3Client player = clients[playerID];
+		clientCount--;
+		clients[playerID] = null;
+		if (boot)
+		{
+			player.print("You have been booted.");
+			messageAll(SYSTEM, player.name() + " has been removed from the game.");
+		}
+		else 
+		{
+			player.print("Thanks for playing!");
+			messageAll(SYSTEM, player.name() + " has left the game.");
+		}
+		
+		
+		if (clientCount < 2)
+			gameOver = true;
+	}
+	
+	
+	
+	
+	/**
+	 * Send a message to one player.
+	 */
+	void message(T3Client sender, T3Client player, String message)
+	{
+		String nameStamp = "[" + sender.name + "(private)]: ";
+		player.print(nameStamp + message);
+	}
+	
+	
+	void message(int senderID, int targetID, String message)
+	{
+		String nameStamp = "[" + clients[senderID].name + "(private)]: ";
+		clients[targetID].print(nameStamp + message);
+	}
+	
+	
+	/**
+	 * Sends a message to all players.
+	 * @param message
+	 */
+	void messageAll(int senderID, String message)
+	{
+		if (0 <= senderID && senderID <= clients.length) {
+			String nameStamp = "[" + clients[senderID] + "]: ";
+			message = nameStamp + message;
+		}
+		for (int i = 0; i < clients.length; i++)
+			if (clients[i] != null)
+				clients[i].print(message);
+	}
+	
+	
+	void addPlayer()
+	{
+		int target = -1;
+		for (int i = 0; target != -1 || i < clients.length; i++)
+			if (clients[i] != null)
+				target = i;
+		
+		if (target == -1) {
+			; // error message: maximum number of players already in game
+			return;
+		}
+		
+		clients[target] = new T3Client(target, (char) ('A'+target));
+		messageAll(SYSTEM, clients[target].name + " has joined the game.");
+		clientCount++;
+	}
+	
+	
+	/**
+	 * Create a string containing information about input commands.
+	 */
+	String helpScreen()
+	{
+		return "\n[helpScreen] Displaying commands";
+	}
+	
+	/**
+	 * Create a string containing information about the current game.
+	 * @return
+	 */
+	String gameStats()
+	{
+		return "\n[gameStats] Displaying game stats";
+	}
+	
+	/**
+	 * Create a string containing information about the connected players.
+	 */
+	String playerStats()
+	{
+		return "\n[playerStats] Displaying player stats";
+	}
+	
+	/**
+	 * Create a string representation of this game's gameboard for display.
 	 * @return
 	 */
 	String drawBoard() 
@@ -237,7 +566,7 @@ public class T3Server implements Runnable
 					spacingOffset += NDimSpacingOffset(n);
 			
 			/* Check for new line */
-			if (i % this.BOARD_SIZE == 0) 
+			if (i % BOARD_SIZE == 0) 
 			{
 				if (i > 0) out += " | ";	// Trailing board edge
 				out += "\n";
